47a800b2d58d844fabcee7a3469dd11afceb3e7c
Jeanclyde Cruz <ignis14311@gmail.com>
2018-11-30 17:39:40 -1000

    added FunctionPaiirPermutation and test method

--- MiscellaneousLibrary/Miscellaneous.cs
+++ MiscellaneousLibrary/Miscellaneous.cs
@@ -1,20 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+//passing functions as a parameter for methods
+//https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/delegates/using-delegates
 
 namespace UtilityLibraries
 {
-    public class Miscellaneous
-    {
+    public class Miscellaneous {
 
-        public static int[] RandomIntegerOrder(int listSize)
-        {
-            int[] result =new int[listSize];
+        /// <summary>
+        /// random order generator function
+        /// 
+        /// generates a list from 0 to 'n'
+        /// iterates through the list starting
+        /// at zero, and swaps with a random index
+        /// 
+        /// </summary>
+        /// <param name="listSize">
+        /// the length of a list size 'n'
+        /// </param>
+        /// <returns>
+        /// an int array containing numbers from
+        /// 0 (inclusive) up to 'n' (exclusive)
+        /// not in order
+        /// </returns>
+        public static int[] RandomIntegerOrder(int listSize) {
+            int[] result = new int[listSize];
 
             //O(n)
-            for(int i = 0; i < listSize; i++)
-            {
+            for (int i = 0; i < listSize; i++) {
                 result[i] = i;
             }
 
@@ -23,18 +38,71 @@ namespace UtilityLibraries
             int randomIndex;
             int temp;
             Random random = new Random();
-            for(int i = 0; i < listSize; i++)
-            {
+            for (int i = 0; i < listSize; i++) {
                 randomIndex = random.Next(listSize);
                 temp = result[i];
                 result[i] = result[randomIndex];
                 result[randomIndex] = temp;
             }
-
-
-
-            return result;           
+            return result;
         }
-        
+
+
+        /// <summary>
+        /// a method used to help prevent infinite loops
+        /// 
+        /// start a given function in a separate thread,
+        /// while the main thread uses a stopwatch to 
+        /// call a timeout if needed
+        /// </summary>
+        /// <param name="function">
+        /// a method (no parameters) which has a loop structure
+        /// </param>
+        /// <param name="timeOut">
+        /// the allowed time for the given function to live
+        /// </param>
+        public static void LoopSafeGuard(Action function, float timeOut) {
+            Console.WriteLine("LoopSafeGuard Start");
+            Stopwatch stopwatch = new Stopwatch();
+
+            ThreadStart childThreadReference = new ThreadStart(function);
+            Thread loopingThread = new Thread(childThreadReference);
+            stopwatch.Start();
+            loopingThread.Start();
+            while (stopwatch.Elapsed.TotalSeconds < timeOut) {
+                //Console.WriteLine("Time remaining:" + (timeOut - stopwatch.Elapsed.TotalSeconds));
+                if (stopwatch.Elapsed.Seconds >= timeOut) {
+                    loopingThread.Abort();
+                }
+
+            }
+            stopwatch.Stop();
+            Console.WriteLine("LoopSafeGuard End");
+        }        
+
+
     }
+
+    //takes in a array of a type
+    //takes in a function 'f(x,y)'
+    //takes each pair of permutations from 
+    //array and calls f(x,y) with the pair
+    //of values as parameters
+    //returns a 2d array of given type filled
+    //with output of function
+    public class Miscellaneous<T> {
+        public static T[,] FunctionPairPermutation(T[] array,Func<T,T,T> function) {
+            T[,] result = new T[array.Length,array.Length];
+            for (int i = 0; i < array.Length; i++) {
+                for(int j=0; j< array.Length; j++) {
+                    //if (i == j) continue;
+
+                    result[i,j]=function(array[i],array[j]);
+                }
+
+            }
+            return result;
+        }
+    }
+
 }
---
+++ MiscellaneousLibraryTest/MiscellaneousLibraryTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilityLibraries;
+
+namespace MiscellaneousLibraryTest
+{
+    [TestClass]
+    public class MiscellaneousLibraryTest
+    {
+        [TestMethod]
+        public void TestCheckForDuplicates()
+        {
+            Random random = new Random();
+            int randomListSize = random.Next(100);
+            int[] testArray = Miscellaneous.RandomIntegerOrder(randomListSize);
+            bool result = true;
+            
+
+            for(int i = 0; i < randomListSize; i++)
+            {
+                for(int j = 0; j < randomListSize; j++)
+                {
+                    if (i == j) continue;
+
+                    if (testArray[i] == testArray[j])
+                    {
+                        result= false;
+                        break;
+                    }
+
+                }
+            }
+            Assert.IsTrue(result);
+
+            foreach (int i in testArray){
+                Console.Write(i + ",");
+            }
+        }
+
+        [TestMethod]
+        public void TestCheckForAllValues() {
+            Random random = new Random();
+            int randomListSize = random.Next(100);
+            int[] testArray = Miscellaneous.RandomIntegerOrder(randomListSize);
+
+            for (int i = 0; i < randomListSize; i++) {
+                for (int j = 0; j <= randomListSize; j++) {
+                    if (testArray[i] == j) {
+                        Console.Write(j + ",");
+                        break;
+                    }
+
+                    if (testArray[i] == randomListSize) {
+                        Assert.Fail();
+                    }
+                }
+            }
+        }
+        
+        [TestMethod]
+        public void TestLoopSafeGuardPassInfiniteFunctionAsParameter() {
+            float timeOut = 2;
+            bool result = false;
+            try {
+                Console.WriteLine("External Function Start");
+                Miscellaneous.LoopSafeGuard( 
+                    ()=> {
+                        while (true) {
+                            result = false;
+                        }
+                    } ,timeOut);
+                Console.WriteLine("External Function End");
+            } finally {
+                //Console.WriteLine("Pass");
+                result = true;
+                Assert.IsTrue(result);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestLoopSafeGuardPassNonInfiniteFunctionAsParameter() {
+            float timeOut = 2;
+            int sleep = 1;
+            bool result = false;
+            try {
+                //Console.WriteLine("External Function Start");
+                Miscellaneous.LoopSafeGuard( 
+                    () => {
+                        Thread.Sleep(sleep);
+                        result = true;
+                        //Console.WriteLine("Pass");
+                    }, timeOut);
+                //Console.WriteLine("External Function End");
+            } finally {
+                Assert.IsTrue(result);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestFunctionPairPermutation() {
+            int[] intArray = {1,2,3,4,5};
+            int[,] result=new int[intArray.Length,intArray.Length];
+            try {
+                result=Miscellaneous<int>.FunctionPairPermutation(
+                    intArray,
+                    (x, y) => {
+                        Console.WriteLine("{0}+{1}={2}", x, y, (x + y));
+                        return (x + y);
+                    });
+
+            } finally {
+                for(int i = 0; i < result.GetLength(0); i++) {
+                    for(int j=0;j < result.GetLength(1); j++) {
+                        Console.WriteLine("result [{0}][{1}]= {2}", i,j,result[i,j]);
+                    }
+                }
+            }
+
+        }
+    }
+}
